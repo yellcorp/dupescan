5f3c088a4528a21c587eccf9d2f4d3b422ae8178
Jim Boswell <jimb@yellcorp.org>
2017-01-05 12:37:49 -0800

    update correlate to work with refactored lib

--- dupescan/correlate.py
+++ dupescan/correlate.py
@@ -1,7 +1,8 @@
 import argparse
 import collections
+from enum import Enum
+import functools
 import itertools
-import os
 import sys
 
 from dupescan import (
@@ -12,7 +13,7 @@ from dupescan import (
 )
 
 
-__all__ = [ "correlate", "generate_report", "run" ]
+__all__ = ("correlate", "generate_report", "run")
 
 
 def get_arg_parser():
@@ -81,171 +82,162 @@ def get_arg_parser():
     return p
 
 
-MATCH = 1
-ADDED = 2
-REMOVED = 4
-ACTION_ORDER = (MATCH, ADDED, REMOVED)
-ALL_ACTIONS = sum(ACTION_ORDER)
+class Action(Enum):
+    match = 1
+    added = 2
+    removed = 4
+
+
+def tap_iterator(member_function, input_iterator):
+    for member in input_iterator:
+        member_function(member)
+        yield member
+
+
 def correlate(root1, root2, verbose=False):
-    origin = collections.defaultdict(int)
+    all_entries = set()
+    ignore_symlinks = lambda e: not e.is_symlink
 
-    def iter_origin():
-        for path, bits in origin.items():
-            if bits & 1:
-                yield path
-            if bits & 2:
-                yield path
+    entry_iter = tap_iterator(
+        all_entries.add,
+        fs.recurse_iterator(
+            (root1, root2),
+            ignore_symlinks,
+            ignore_symlinks
+        )
+    )
 
-    ignore_symlinks = lambda f: not os.path.islink(f)
-    for root, bit in ((root1, 1), (root2, 2)):
-        for path in fs.recurse_iterator((root,), ignore_symlinks, ignore_symlinks):
-            origin[path] |= bit
+    find_dupes = algo.DuplicateFinder(
+        # buffer_size = buffer_size,
+    )
 
-    for dupe_set in algo.find_duplicate_files(
-        iter_origin(),
-        collect_inodes=False,
-        unique_paths=False,
-        error_cb="print_stderr",
-        log_cb="print_stderr" if verbose else None
-    ):
-        partitions = [ [ ], [ ] ]
+    for dupe_set in find_dupes(entry_iter):
+        partitions = ([ ], [ ])
         for content in dupe_set:
-            path = content.path()
-
-            if origin[path] & 1:
-                partitions[0].append(path)
-                origin[path] &= ~1
-
-            elif origin[path] & 2:
-                partitions[1].append(path)
-                origin[path] &= ~2
-
-            if origin[path] == 0:
-                del origin[path]
+            partitions[content.entry.root_index].append(content.entry)
+            all_entries.remove(content.entry)
 
         for a, b in itertools.zip_longest(*partitions):
+            assert not (a is None and b is None), "got (None, None) from zip_longest"
+
             if a is None:
-                # in this case, b cannot be None. b was added
-                assert b is not None, "zip_longest somehow yielded (None, None)"
-                action = ADDED
-            else: # a is not None
-                if b is None:
-                    # a was removed
-                    action = REMOVED
-                else:
-                    # a and b match
-                    action = MATCH
+                action = Action.added
+            elif b is None:
+                action = Action.removed
+            else:
+                action = Action.match
 
-            yield (action, a, b)
+            yield action, a, b
 
-    # origin paths are removed as they are encountered. all that remains at
-    # this point will be files that weren't matched in the other root
-    for path, bit in origin.items():
-        assert bit not in (0, 3), "{!r} has invalid value {}".format(path, bit)
-        if bit == 1:
-            yield (REMOVED, path, None)
+    for entry in all_entries:
+        assert entry.root_index in (0, 1), "bad root_index while iterating unmatched entries"
+        if entry.root_index == 0:
+            yield Action.removed, entry, None
         else:
-            yield (ADDED, None, path)
+            yield Action.added, None, entry
 
 
 ACTION_STRINGS = {
-    MATCH:   ("=", "Matches", None),
-    ADDED:   ("+", "Adds",    "32"),
-    REMOVED: ("-", "Removes", "31")
+    Action.match:   ("matches", "=", "Matches", None),
+    Action.added:   ("adds",    "+", "Adds",    "32"),
+    Action.removed: ("removes", "-", "Removes", "31")
 }
-SYMBOL = 0
-SUMMARY_WORD = 1
-DEFAULT_SGR = 2
+ARG_NAME = 0
+SYMBOL = 1
+SUMMARY_WORD = 2
+DEFAULT_SGR = 3
 
 
 def format_ansi_sgr(string, sgr):
     if sgr is None:
         return string
-    return "\x1b[{}m{}\x1b[0m".format(sgr, string)
+    return "\x1b[%sm%s\x1b[0m" % (sgr, string)
+
+
+def interpret_ansi_param(ansi, out_stream):
+    if ansi is None:
+        try:
+            ansi = out_stream.isatty()
+        except AttributeError:
+            ansi = False
+
+    if ansi is True:
+        return {
+            action: strings[DEFAULT_SGR]
+            for action, strings in ACTION_STRINGS.items()
+        }
+
+    if ansi is False:
+        return {
+            action: None
+            for action in Action
+        }
+
+    return dict(zip(Action, ansi))
 
 
 def generate_report(
     root1, root2,
-    filter_actions=ALL_ACTIONS,
+    include_actions=None,
     ansi=None, # None: autodetect, True: use default, False: no colors, or tuple of 3 things to appear between \x1b[ and m
     summary=True,
     verbose=False,
     file=None
 ):
+    if include_actions is None or len(include_actions) == 0:
+        include_actions = set(Action)
+    else:
+        include_actions = set(include_actions)
+
     if file is None:
         file = sys.stdout
+    out = functools.partial(print, file=file)
 
-    if ansi is None:
-        try:
-            ansi = file.isatty()
-        except AttributeError:
-            ansi = False
-
-    if ansi is True:
-        ansi_codes = tuple(
-            ACTION_STRINGS[action][DEFAULT_SGR]
-            for action in ACTION_ORDER
-        )
-    elif ansi is False:
-        ansi_codes = (None, None, None)
-    else:
-        ansi_codes = tuple(ansi)
-
-    sgr_lookup = dict(zip(ACTION_ORDER, ansi_codes))
+    sgr_lookup = interpret_ansi_param(ansi, file)
 
     action_count = collections.Counter()
-
-    for action, path1, path2 in correlate(root1, root2, verbose=verbose):
+    for action, entry1, entry2 in correlate(root1, root2, verbose=verbose):
         action_count[action] += 1
-        if filter_actions & action:
-            symbol = ACTION_STRINGS[action][SYMBOL]
 
-            if path1 is not None:
-                print(format_ansi_sgr(
-                    "{} {}".format(symbol, report.format_path(path1)),
-                    sgr_lookup[action]
-                ), file=file)
-                symbol = " "
+        if action not in include_actions:
+            continue
 
-            if path2 is not None:
-                print(format_ansi_sgr(
-                    "{} {}".format(symbol, report.format_path(path2)),
-                    sgr_lookup[action]
-                ), file=file)
+        symbol = ACTION_STRINGS[action][SYMBOL]
+        for entry in (entry1, entry2):
+            if entry is None:
+                continue
+            out(format_ansi_sgr(
+                "%s %s" % (symbol, report.format_path(entry.path)),
+                sgr_lookup[action]
+            ))
+            symbol = " "
 
-            print()
+        out("")
 
     if summary:
-        print(
-            "# " +
-            ", ".join(
-                "{}: {}".format(ACTION_STRINGS[action][SUMMARY_WORD], action_count[action])
-                for action in (MATCH, ADDED, REMOVED)
-            ),
-            file=file
+        counts = (
+            "%s: %s" % (ACTION_STRINGS[action][SUMMARY_WORD], action_count[action])
+            for action in Action
         )
+        out("# " + ", ".join(counts))
 
 
 def run(argv=None):
     p = get_arg_parser()
     args = p.parse_args(argv)
 
-    filter_actions = sum(
-        action for
-        action, select in (
-            (MATCH,   args.matches),
-            (ADDED,   args.adds),
-            (REMOVED, args.removes)
-        )
-        if select
+    include_actions = set(
+        action
+        for action, strings in ACTION_STRINGS.items()
+        if getattr(args, strings[ARG_NAME])
     )
 
-    if filter_actions == 0:
-        filter_actions = ALL_ACTIONS
+    if len(include_actions) == 0:
+        include_actions = set(Action)
 
     generate_report(
         *args.dirs,
-        filter_actions=filter_actions,
+        include_actions=include_actions,
         ansi=args.colorize,
         summary=not args.no_summary,
         verbose=args.verbose,
