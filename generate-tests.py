c8b854c13cd0363c54d12498babf7bad2eeae56f
Jim Boswell <jimb@yellcorp.org>
2017-01-05 12:53:41 -0800

    generate separate set of test files for correlate

--- generate-tests.py
+++ generate-tests.py
@@ -23,8 +23,7 @@ BIG_SIZE = 30 * 1024 * 1024 + 5 # 30 MB + 5 bytes
 SMALL_SIZE = 3 * 1024 * 1024 + 5 # 3 MB + 5 bytes
 
 
-def main():
-    base_dir = sys.argv[1]
+def generate_finddupes(base_dir):
     root_a = os.path.join(base_dir, "root_a")
     root_b = os.path.join(base_dir, "root_b")
 
@@ -75,6 +74,53 @@ def main():
     buf = bytearray(b"R" * SMALL_SIZE)
     write_files(buf, root_a, "oar_a_{}", 0)
     write_files(buf, root_b, "oar_b_{}", 0)
+
+
+def generate_correlate(base_dir):
+    root_a = os.path.join(base_dir, "root_a")
+    root_b = os.path.join(base_dir, "root_b")
+
+    # test simple dupe case
+    buf = bytearray(b"Z" * SMALL_SIZE)
+    write_files(buf, root_a, "simple_{}", 0)
+    write_files(buf, root_b, "simple_{}", 0)
+
+    # test dupe within tree and across tree
+    buf = bytearray(b"Y" * SMALL_SIZE)
+    write_files(buf, root_a, "same_count_{}", 1)
+    write_files(buf, root_b, "same_count_{}", 1)
+
+    # test dupe with more dupes in a than b
+    buf = bytearray(b"X" * SMALL_SIZE)
+    write_files(buf, root_a, "more_a_{}", 2)
+    write_files(buf, root_b, "more_a_{}", 1)
+
+    # test dupe with more dupes in b than a
+    buf = bytearray(b"W" * SMALL_SIZE)
+    write_files(buf, root_a, "more_b_{}", 1)
+    write_files(buf, root_b, "more_b_{}", 2)
+
+    # test dupe with only dupes in a
+    buf = bytearray(b"V" * SMALL_SIZE)
+    write_files(buf, root_a, "a_only_{}", 1)
+
+    # test dupe with only dupes in b
+    buf = bytearray(b"U" * SMALL_SIZE)
+    write_files(buf, root_b, "b_only_{}", 1)
+
+    # test singleton only in a
+    buf = bytearray(b"T" * SMALL_SIZE)
+    write_files(buf, root_a, "a_singleton_{}", 0)
+
+    # test singleton only in b
+    buf = bytearray(b"S" * SMALL_SIZE)
+    write_files(buf, root_b, "b_singleton_{}", 0)
+
+
+def main():
+    base_dir = sys.argv[1]
+    generate_finddupes(os.path.join(base_dir, "finddupes"))
+    generate_correlate(os.path.join(base_dir, "correlate"))
 
 
 if __name__ == "__main__":
