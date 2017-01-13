8339a9fa5f92e034a8b97238d8b6275064c57cc0
Jim Boswell <jimb@yellcorp.org>
2017-01-13 14:57:18 -0800

    publicize some stuff from core

--- dupescan/__init__.py
+++ dupescan/__init__.py
@@ -3,3 +3,9 @@ Library and command-line utilities to detect duplicate files by content.
 """
 
 __version__ = "0.0.10-develop"
+
+
+from dupescan.core import (
+    DuplicateFinder,
+    DuplicateContentSet,
+)
